69c5bc614bc55388fdfe6ca86ed2fb1781fdaed3
Scott Morken <smorken@nrcan.gc.ca>
2018-05-28 17:02:20 -0700

    dm update

--- adhoc/update_oilsands_dms.py
+++ adhoc/update_oilsands_dms.py
@@ -16,11 +16,18 @@ dms = excelhelper.get_worksheet_as_dict(
     os.path.join(local_wd, "DRAFT_DMS_FOR_FIRST_PILOT_STUDY_RUN_DEC_16_2016_VScott.xlsx"),
     2)
 
-dms_by_language = {d["Language"]: d for d in dms}
+
+dms_by_language = {}
+for d in dms:
+    #group by language
+    if d["Language"] in dms_by_language:
+        dms_by_language[d["Language"]].append(d)
+    else:
+        dms_by_language[d["Language"]] = [d]
 
 original_aidbs = [
-    { "Language": "English", "Path": r"M:\CBM Tools and Development\Builds\OpScaleArchiveIndex\ArchiveIndex_Beta_Install.mdb" },
-    { "Language": "French", "Path": r"M:\CBM Tools and Development\Builds\OpScaleArchiveIndex\ArchiveIndex_Beta_Install_fr.mdb" },
+    { "Language": "English", "Path": r"M:\CBM Tools and Development\Builds\OpScaleArchiveIndex\20180525\ArchiveIndex_Beta_Install.mdb" },
+    { "Language": "French", "Path": r"M:\CBM Tools and Development\Builds\OpScaleArchiveIndex\20180525\ArchiveIndex_Beta_Install_fr.mdb" },
 ]
 makeLocalPath = lambda path : os.path.join(local_wd, os.path.basename(path))
 local_aidbs = [{"Language": x["Language"], "Path": makeLocalPath(x["Path"])} for x in original_aidbs]
@@ -34,13 +41,23 @@ for i in range(len(original_aidbs)):
     shutil.copy(origPath, localPath)
 
 #add the disturbance types
-
-
 for p in local_aidbs:
     logging.info("adding oilsands DM to {0}".format(p))
     with AccessDB(p["Path"], False) as a:
-        #disturbance types (tblDisturbanceTypeDefault)
+        language = p["Language"]
 
+        nextDistTypeID = a.GetMaxID("tblDisturbanceTypeDefault", "DistTypeID") + 1
+        #disturbance types (tblDisturbanceTypeDefault)
+        for r in dms_by_language[language]:
+            a.ExecuteQuery("INSERT INTO tblDisturbanceTypeDefault (DistTypeID, DistTypeName, OnOffSwitch, Description, IsStandReplacing, IsMultiYear, MultiYearCount) VALUES (?,?,?,?,?,?,?)", 
+                          (nextDistTypeID,
+                          r["Name"],
+                          True,
+                          r["Description"],
+                          True,
+                          False,
+                          0))
+            nextDistTypeID += 1
         #disturbance matrices (tblDM)
 
         #disturbance matrix values (tblDMValuesLookup)
