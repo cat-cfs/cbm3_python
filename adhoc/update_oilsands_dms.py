088ce3453573b85c03d00895e0e69ee05c8ac185
Scott Morken <smorken@nrcan.gc.ca>
2018-06-05 10:30:30 -0700

    updated oilsands matrix spreadsheet

--- adhoc/update_oilsands_dms.py
+++ adhoc/update_oilsands_dms.py
@@ -24,6 +24,17 @@ for d in dms:
     else:
         dms_by_language[d["Language"]] = [d]
 
+dm_values = excelhelper.get_worksheet_as_dict(
+    os.path.join(local_wd, "Oilsands_DM_update.xlsx"),
+    1)
+
+dm_values_by_id = {}
+for d in dm_values:
+    if d["DMID"] in dm_values_by_id:
+        dm_values_by_id[d["DMID"]].append(d)
+    else:
+        dm_values_by_id[d["DMID"]] = [d]
+
 original_aidbs = [
     { "Language": "English", "Path": r"M:\CBM Tools and Development\Builds\OpScaleArchiveIndex\20180525\ArchiveIndex_Beta_Install.mdb" },
     { "Language": "Spanish", "Path": r"M:\CBM Tools and Development\Builds\OpScaleArchiveIndex\20180525\ArchiveIndex_Beta_Install_es.mdb" },
@@ -47,6 +58,9 @@ for p in local_aidbs:
     with AccessDB(p["Path"], False) as a:
         language = p["Language"]
 
+        # get set of eco boundaries. Though the matrices apply to QC only, we are associating it with all ecozones for the time being.
+        source_ecozones = [int(x[0]) for x in a.Query("SELECT tblEcoBoundaryDefault.EcoBoundaryID from tblEcoBoundaryDefault")]
+
         nextDistTypeID = a.GetMaxID("tblDisturbanceTypeDefault", "DistTypeID") + 1
         nextDMID = a.GetMaxID("tblDM", "DMID") + 1
         #disturbance types (tblDisturbanceTypeDefault)
@@ -61,19 +75,33 @@ for p in local_aidbs:
                           0))
 
             #disturbance matrices (tblDM)
-            for dmrow in dms_by_language[language]:
-                logging.info(dmrow.Name)
-                a.ExecuteQuery("INSERT INTO tblDM (DMID, Name, Description, DMStructureID) VALUES (?,?,?,?)",
-                               (nextDMID,
-                               r["Name"],
-                               r["Description"],
-                               2))
+            a.ExecuteQuery("INSERT INTO tblDM (DMID, Name, Description, DMStructureID) VALUES (?,?,?,?)",
+                (nextDMID,
+                r["Name"],
+                r["Description"],
+                2))
+
+            dmid = int(r["DMID"])
+            dm_value_rows = dm_values_by_id[dmid]
+            for dm_value_row in dm_value_rows:
+                a.ExecuteQuery("INSERT INTO tblDMValuesLookup (DMID, DMRow, DMColumn, Proportion) VALUES (?,?,?,?)",
+                    (nextDMID,
+                    int(dm_value_row["DMRow"]),
+                    int(dm_value_row["DMColumn"]),
+                    dm_value_row["Proportion"]))
+
+            for eco in source_ecozones:
+                a.ExecuteQuery("INSERT INTO tblDMAssociationDefault (DefaultDisturbanceTypeID, DefaultEcoBoundaryID, AnnualOrder, DMID, Name, Description) VALUES (?,?,?,?,?,?)",
+                            (nextDistTypeID,
+                            eco,
+                            1,
+                            nextDMID,
+                            r["Name"],
+                            r["Description"]))
 
             nextDMID += 1
             nextDistTypeID += 1
 
-
-
-        #disturbance matrix values (tblDMValuesLookup)
-
-        #disturbance matrix association (tblDMAssociationDefault)
+for p in local_aidbs:
+    logging.info("run compact and repair on {}".format(p["Path"]))
+    compact_and_repair(p["Path"])
