6a1b72b265886284fda31fe2bf97be2083c6e44c
Scott Morken <smorken@nrcan.gc.ca>
2018-09-24 15:10:20 -0700

    arg parse, logging, exception handling

--- simulate.py
+++ simulate.py
@@ -1,39 +1,69 @@
-import os, shutil
+import os, shutil, argparse, datetime, logging
 from simulation.simulator import Simulator
 from cbm3data.aidb import AIDB
 from cbm3data.accessdb import AccessDB
 from cbm3data.projectdb import ProjectDB
-from cbm3data.resultsloader import ResultsLoader
+
+from util import loghelper
+def main():
+    try:
+        logpath = os.path.join(
+                    "{0}_{1}.log".format(
+                        "simulation",
+                        datetime.datetime.now().strftime("%Y-%m-%d %H_%M_%S")))
+        loghelper.start_logging(logpath, 'w+')
+
+        parser = argparse.ArgumentParser(description="CBM-CFS3 simulation "
+            "script. Simulates a CBM-CFS3 project access database by "
+            "automating functions in the Operational-Scale CBM-CFS3 toolbox")
+        parser.add_argument("--projectdb", help="path to a cbm project database file")
+        parser.add_argument("--toolbox_path", nargs="?",
+                            help="the Operational-Scale CBM-CFS3 toolbox "
+                            "installation directory. If unspecified a the "
+                            "typical default value is used.")
+        args = parser.parse_args()
+
+        toolbox_installation_dir = r"C:\Program Files (x86)\Operational-Scale CBM-CFS3" \
+            if not args.toolbox_path else os.path.abspath(args.toolbox_path)
+
+        aidb_path = os.path.join(toolbox_installation_dir, "admin", "dbs", "ArchiveIndex_Beta_Install.mdb")
+        cbm_exe_path = os.path.join(toolbox_installation_dir, "admin", "executables")
+        working_aidb_path = os.path.join(toolbox_installation_dir, "admin", "dbs", "ArchiveIndex_Beta_Install_working.mdb")
+        shutil.copyfile(aidb_path, working_aidb_path)
+
+        project_path = os.path.abspath(args.projectdb)
+
+        with AIDB(working_aidb_path, False) as aidb, \
+             AccessDB(project_path, False) as proj:
+            aidb.DeleteProjectsFromAIDB()
+            simId = aidb.AddProjectToAIDB(proj)
+            cbm_wd = os.path.join(toolbox_installation_dir, "temp")
+            s = Simulator(cbm_exe_path,
+                            simId,
+                            os.path.dirname(project_path),
+                            cbm_wd,
+                            toolbox_installation_dir)
+
+            s.CleanupRunDirectory()
+            s.CreateMakelistFiles()
+            s.copyMakelist()
+            s.runMakelist()
+            s.loadMakelistSVLS()
+            s.DumpMakelistSVLs()
+            s.copyMakelistOutput()
+            s.CreateCBMFiles()
+            s.CopyCBMExecutable()
+            s.RunCBM()
+            s.CopyTempFiles()
+            s.LoadCBMResults()
+            results_path = s.getResultsPath()
+            logging.info("simulation finish, results path: {0}"
+                         .format(results_path))
+
+            
+    except Exception as ex:
+        logging.exception("")
+
 
 if __name__ == '__main__':
-    import argparse
-
-    toolbox_installation_dir = r"C:\Program Files (x86)\Operational-Scale CBM-CFS3"
-    aidb_path = os.path.join(toolbox_installation_dir, "admin", "dbs", "ArchiveIndex_Beta_Install.mdb")
-    cbm_exe_path = os.path.join(toolbox_installation_dir, "admin", "executables")
-    working_aidb_path = os.path.join(toolbox_installation_dir, "admin", "dbs", "ArchiveIndex_Beta_Install_working.mdb")
-    shutil.copyfile(aidb_path, working_aidb_path)
-    project_path = r"C:\Program Files (x86)\Operational-Scale CBM-CFS3\Tutorials\Tutorial 3\Tutorial3.mdb"
-    with AIDB(working_aidb_path, False) as aidb, \
-         AccessDB(project_path, False) as proj:
-        aidb.DeleteProjectsFromAIDB()
-        simId = aidb.AddProjectToAIDB(proj)
-        cbm_wd = os.path.join(toolbox_installation_dir, "temp")
-        s = Simulator(cbm_exe_path,
-                        simId,
-                        os.path.dirname(project_path),
-                        cbm_wd,
-                        toolbox_installation_dir)
-
-        s.CleanupRunDirectory()
-        s.CreateMakelistFiles()
-        s.copyMakelist()
-        s.runMakelist()
-        s.loadMakelistSVLS()
-        s.DumpMakelistSVLs()
-        s.copyMakelistOutput()
-        s.CreateCBMFiles()
-        s.CopyCBMExecutable()
-        s.RunCBM()
-        s.CopyTempFiles()
-        s.LoadCBMResults()
+    main()
