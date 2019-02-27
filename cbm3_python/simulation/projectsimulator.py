ad8f76f8d9b7929040a434c63c54e2486d276e25
Scott Morken <smorken@nrcan.gc.ca>
2019-02-27 15:07:06 -0800

    making command line script thinner, and allow use of related function externally as module

---
+++ cbm3_python/simulation/projectsimulator.py
@@ -0,0 +1,34 @@
+from cbm3_python.cbm3data.aidb import AIDB
+from cbm3_python.cbm3data.accessdb import AccessDB
+from cbm3_python.cbm3data.projectdb import ProjectDB
+
+def run(aidb_path, project_path, toolbox_installation_dir, cbm_exe_path,
+       results_database_path=None, tempfiles_output_dir=None):
+    with AIDB(aidb_path, False) as aidb, \
+         AccessDB(project_path, False) as proj:
+
+        simId = aidb.AddProjectToAIDB(proj)
+        try:
+            cbm_wd = os.path.join(toolbox_installation_dir, "temp")
+            s = Simulator(cbm_exe_path,
+                            simId,
+                            os.path.dirname(project_path),
+                            cbm_wd,
+                            toolbox_installation_dir)
+
+            s.CleanupRunDirectory()
+            s.CreateMakelistFiles()
+            s.copyMakelist()
+            s.runMakelist()
+            s.loadMakelistSVLS()
+            s.DumpMakelistSVLs()
+            s.CreateCBMFiles()
+            s.CopyCBMExecutable()
+            s.RunCBM()
+            s.CopyTempFiles(output_dir=tempfiles_output_dir)
+            s.LoadCBMResults(output_path = results_database_path)
+        finally:
+            aidb.DeleteProjectsFromAIDB(simId) #cleanup
+        results_path = s.getDefaultResultsPath() if results_database_path is None else results_database_path
+        return results_path
+
--- simulate.py
+++ simulate.py
@@ -2,40 +2,10 @@
 #  as represented by the Minister of Natural Resources Canada
 
 import os, shutil, argparse, datetime, logging
-from cbm3_python.simulation.simulator import Simulator
-from cbm3_python.cbm3data.aidb import AIDB
-from cbm3_python.cbm3data.accessdb import AccessDB
-from cbm3_python.cbm3data.projectdb import ProjectDB
+import cbm3_python.simulation.projectsimulator
+
 from cbm3_python.util import loghelper
-def run(aidb_path, project_path, toolbox_installation_dir, cbm_exe_path,
-       results_database_path=None, tempfiles_output_dir=None):
-    with AIDB(aidb_path, False) as aidb, \
-         AccessDB(project_path, False) as proj:
 
-        simId = aidb.AddProjectToAIDB(proj)
-        try:
-            cbm_wd = os.path.join(toolbox_installation_dir, "temp")
-            s = Simulator(cbm_exe_path,
-                            simId,
-                            os.path.dirname(project_path),
-                            cbm_wd,
-                            toolbox_installation_dir)
-
-            s.CleanupRunDirectory()
-            s.CreateMakelistFiles()
-            s.copyMakelist()
-            s.runMakelist()
-            s.loadMakelistSVLS()
-            s.DumpMakelistSVLs()
-            s.CreateCBMFiles()
-            s.CopyCBMExecutable()
-            s.RunCBM()
-            s.CopyTempFiles(output_dir=tempfiles_output_dir)
-            s.LoadCBMResults(output_path = results_database_path)
-        finally:
-            aidb.DeleteProjectsFromAIDB(simId) #cleanup
-        results_path = s.getDefaultResultsPath() if results_database_path is None else results_database_path
-        return results_path
 
 def main():
     try:
@@ -70,7 +40,7 @@ def main():
         cbm_exe_path = os.path.join(toolbox_installation_dir, "admin", "executables")
         project_path = os.path.abspath(args.projectdb)
         
-        results_path = run(aidb_path, project_path, toolbox_installation_dir, cbm_exe_path,
+        results_path = projectsimulator.run(aidb_path, project_path, toolbox_installation_dir, cbm_exe_path,
                            results_database_path=results_db_path,
                            tempfiles_output_dir=tempfiles_output_dir)
         logging.info("simulation finish, results path: {0}"
