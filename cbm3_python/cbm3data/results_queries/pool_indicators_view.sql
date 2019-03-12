4b855d7707f4b23cef642cd708f93e6a1ebf2bf2
Scott Morken <smorken@nrcan.gc.ca>
2019-03-12 11:38:18 -0700

    drop submerch (unused pools in cbm3) add descriptive pool names

--- cbm3_python/cbm3data/results_queries/pool_indicators_view.sql
+++ cbm3_python/cbm3data/results_queries/pool_indicators_view.sql
@@ -1,44 +1,42 @@
 SELECT 
 tpi.TimeStep,{0}
-sum(tpi.VFastAG) as VFastAG,
-sum(tpi.VFastBG) as VFastBG,
-sum(tpi.FastAG) as FastAG,
-sum(tpi.FastBG) as FastBG,
-sum(tpi.Medium) as Medium,
-sum(tpi.SlowAG) as SlowAG,
-sum(tpi.SlowBG) as SlowBG,
-sum(tpi.SWStemSnag) as SWStemSnag,
-sum(tpi.SWBranchSnag) as SWBranchSnag,
-sum(tpi.HWStemSnag) as HWStemSnag,
-sum(tpi.HWBranchSnag) as HWBranchSnag,
+sum(tpi.VFastAG) as [Aboveground Very Fast DOM],
+sum(tpi.VFastBG) as [Belowground Very Fast DOM],
+sum(tpi.FastAG) as [Aboveground Fast DOM],
+sum(tpi.FastBG) as [Belowground Fast DOM],
+sum(tpi.Medium) as [Medium DOM],
+sum(tpi.SlowAG) as [Aboveground Slow DOM],
+sum(tpi.SlowBG) as [Belowground Slow DOM],
+sum(tpi.SWStemSnag) as [Softwood Stem Snag],
+sum(tpi.SWBranchSnag) as [Softwood Branch Snag],
+sum(tpi.HWStemSnag) as [Hardwood Stem Snag],
+sum(tpi.HWBranchSnag) as [Hardwood Branch Snag],
 sum(tpi.BlackCarbon) as BlackCarbon,
 sum(tpi.Peat) as Peat,
-sum(tpi.SW_Merch) as SW_Merch,
-sum(tpi.SW_Foliage) as SW_Foliage,
-sum(tpi.SW_Other) as SW_Other,
-sum(tpi.SW_subMerch) as SW_subMerch,
-sum(tpi.SW_Coarse) as SW_Coarse,
-sum(tpi.SW_Fine) as SW_Fine,
-sum(tpi.HW_Merch) as HW_Merch,
-sum(tpi.HW_Foliage) as HW_Foliage,
-sum(tpi.HW_Other) as HW_Other,
-sum(tpi.HW_subMerch) as HW_subMerch,
-sum(tpi.HW_Coarse) as HW_Coarse,
-sum(tpi.HW_Fine) as HW_Fine,
-sum(tpi.SW_Merch+tpi.SW_Foliage+tpi.SW_Other+tpi.SW_subMerch+tpi.SW_Coarse
+sum(tpi.SW_Merch) as [Softwood Merchantable],
+sum(tpi.SW_Foliage) as [Softwood Foliage],
+sum(tpi.SW_Other) as [Softwood Other],
+sum(tpi.SW_Coarse) as [Softwood Coarse Roots],
+sum(tpi.SW_Fine) as [Softwood Fine Roots],
+sum(tpi.HW_Merch) as [Hardwood Merchantable],
+sum(tpi.HW_Foliage) as [Hardwood Foliage],
+sum(tpi.HW_Other) as [Hardwood Other],
+sum(tpi.HW_Coarse) as [Hardwood Coarse Roots],
+sum(tpi.HW_Fine) as [Hardwood Find Roots],
+sum(tpi.SW_Merch+tpi.SW_Foliage+tpi.SW_Other+tpi.SW_Coarse
     +tpi.SW_Fine+tpi.HW_Merch+tpi.HW_Foliage+tpi.HW_Other 
-    +tpi.HW_subMerch+tpi.HW_Coarse+tpi.HW_Fine) as [Total Biomass],
-sum(tpi.SW_Merch+tpi.SW_Foliage+tpi.SW_Other+tpi.SW_subMerch+ 
-    tpi.HW_Merch+tpi.HW_Foliage+tpi.HW_Other +tpi.HW_subMerch) 
+    +tpi.HW_Coarse+tpi.HW_Fine) as [Total Biomass],
+sum(tpi.SW_Merch+tpi.SW_Foliage+tpi.SW_Other+
+    tpi.HW_Merch+tpi.HW_Foliage+tpi.HW_Other ) 
     as [Aboveground Biomass],
 sum(tpi.SW_Coarse+tpi.SW_Fine+tpi.HW_Coarse+tpi.HW_Fine) 
     as [Belowground Biomass],
 sum(tpi.VFastAG+tpi.VFastBG+tpi.FastAG+tpi.FastBG+tpi.Medium+tpi.SlowAG
     +tpi.SlowBG+tpi.SWStemSnag+tpi.SWBranchSnag+tpi.HWStemSnag
     +tpi.HWBranchSnag+tpi.BlackCarbon+ tpi.Peat+tpi.SW_Merch
-    +tpi.SW_Foliage+tpi.SW_Other+tpi.SW_subMerch+tpi.SW_Coarse
+    +tpi.SW_Foliage+tpi.SW_Other+tpi.SW_Coarse
     +tpi.SW_Fine+tpi.HW_Merch+tpi.HW_Foliage+tpi.HW_Other 
-    +tpi.HW_subMerch+tpi.HW_Coarse+tpi.HW_Fine) as [Total Ecosystem],
+    +tpi.HW_Coarse+tpi.HW_Fine) as [Total Ecosystem],
 sum(tpi.VFastAG+tpi.VFastBG+tpi.FastAG+tpi.FastBG+tpi.Medium+tpi.SlowAG
     +tpi.SlowBG+tpi.SWStemSnag+tpi.SWBranchSnag+tpi.HWStemSnag
     +tpi.HWBranchSnag+tpi.BlackCarbon+ tpi.Peat) as [Dead Organic Matter],
