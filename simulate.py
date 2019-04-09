3b427bc6696b6314534a514f2187be0a8f2535e5
Scott Morken <smorken@nrcan.gc.ca>
2019-04-09 14:10:47 -0700

    add support to CLI for new stdout_path option

--- simulate.py
+++ simulate.py
@@ -32,12 +32,17 @@ def main():
         parser.add_argument("--afforestation_only", nargs="?",
                     help="if the specified projectdb contains only initially "
                     "non-forested stands, set this option to true")
+        parser.add_argument("--stdout_path", nargs="?",
+                    help="optionally redirect makelist and CBM standard "
+                    "output to the specified file. If unspecified standard "
+                    "out is directed to the console window")
         args = parser.parse_args()
 
         toolbox_installation_dir = r"C:\Program Files (x86)\Operational-Scale CBM-CFS3" \
             if not args.toolbox_path else os.path.abspath(args.toolbox_path)
         results_db_path = None if not args.results_db_path else os.path.abspath(args.results_db_path)
         tempfiles_output_dir = None if not args.tempfiles_output_dir else os.path.abspath(args.tempfiles_output_dir)
+        stdout_path = None if not args.stdout_path else os.path.abspath(args.stdout_path)
 
         aidb_path = os.path.join(toolbox_installation_dir, "admin", "dbs", "ArchiveIndex_Beta_Install.mdb")
         cbm_exe_path = os.path.join(toolbox_installation_dir, "admin", "executables")
@@ -47,7 +52,8 @@ def main():
             aidb_path, project_path, toolbox_installation_dir, cbm_exe_path,
                            results_database_path=results_db_path,
                            tempfiles_output_dir=tempfiles_output_dir,
-                           afforestation_only=afforestation_only)
+                           afforestation_only=afforestation_only,
+                           stdout_path=stdout_path)
         logging.info("simulation finish, results path: {0}"
                         .format(results_path))
 
