b092f2b8a304f6181f46c33fbd0e090389f4888c
Scott Morken <smorken@nrcan.gc.ca>
2019-04-12 16:09:21 -0700

    version update

--- cbm3_python/cbm3data/sit_helper.py
+++ cbm3_python/cbm3data/sit_helper.py
@@ -8,13 +8,13 @@ from zipfile import ZipFile
 
 def load_standard_import_tool_plugin():
     '''
-    Download the 1.2.1 release of StandardImportToolPlugin from Github and unzip it locally.
+    Download the 1.2.2.1 release of StandardImportToolPlugin from Github and unzip it locally.
     '''
     StandardImportToolPluginDir = os.path.join(".","StandardImportToolPlugin")
     #extra subdir in the archive
     StandardImportToolPluginExe = os.path.join(StandardImportToolPluginDir,"Release", "StandardImportToolPlugin.exe")
     if not os.path.exists(StandardImportToolPluginExe):
-        resp = urlopen('https://github.com/cat-cfs/StandardImportToolPlugin/releases/download/1.2.2/Release.zip')
+        resp = urlopen('https://github.com/cat-cfs/StandardImportToolPlugin/releases/download/1.2.2.1/Release.zip')
         zipfile = ZipFile(BytesIO(resp.read()))
 
         #os.makedirs(StandardImportToolPluginDir)
