890bfdcf4bd99bd742903eccb4edaf4d2d0223c2
Scott Morken <smorken@nrcan.gc.ca>
2019-05-07 13:29:08 -0700

    cleanup trailing spaces

--- cbm3_python/simulation/tools/dgchecker.py
+++ cbm3_python/simulation/tools/dgchecker.py
@@ -18,7 +18,7 @@ class DGChecker(object):
         '''
         Constructor
         '''   
-        self.tasks = tasks        
+        self.tasks = tasks
         self.event_queries = self.__load_dg_metadata(dg_default_path)
         
         #query the event database to get alll of the event data
@@ -30,7 +30,7 @@ class DGChecker(object):
         result = []
         
         for d in root[0].findall("DGDatasource"):
-            dist_class = d.attrib["DisturbanceClass"] 
+            dist_class = d.attrib["DisturbanceClass"]
             
             for q in d.find("Queries").findall("Query"):
                 name = q.attrib["Name"]
@@ -42,57 +42,61 @@ class DGChecker(object):
       
     def __query_source_disturbance_data (self):
         for task in self.tasks:
-            dist_class = task["DisturbanceClass"]            
+            dist_class = task["DisturbanceClass"]
             disturbance_DBPath = task["DisturbanceDBPath"]
             
-            query = self.__get_query_by_distclass(dist_class)       
+            query = self.__get_query_by_distclass(dist_class)
             
-            #record the event start year to be used to query project events     
+            #record the event start year to be used to query project events
             self.event_start_year = query[-4:]
             
             #for each task, query the event database
             with AccessDB(disturbance_DBPath) as event_db:
                 if dist_class == "ForestManagement":
-                    self.fm_df = pd.read_sql(query, event_db.connection)        
+                    self.fm_df = pd.read_sql(query, event_db.connection)
                 elif dist_class == "Bioenergy":
-                    self.bio_df = pd.read_sql(query, event_db.connection)              
-                    
+                    self.bio_df = pd.read_sql(query, event_db.connection)
+
+
     def __get_query_by_distclass(self, dist_class):
         for item in self.event_queries:
             if item["dist_class"] == dist_class:
                 return item["query"]
-    
-    def __get_event_data(self, dist_class, project_prefix):   
+
+
+    def __get_event_data(self, dist_class, project_prefix):
         if dist_class == "ForestManagement":
             df_events = self.fm_df.loc[self.fm_df["ProjectName"] == project_prefix]
         elif dist_class == "Bioenergy":
             df_events = self.bio_df.loc[self.bio_df["ProjectName"] == project_prefix]
         return df_events 
-    
+
+
     def __get_project_default_disturbance_ids(self, dist_class, project_prefix):
-        df_events = self.__get_event_data(dist_class, project_prefix)   
-        unique_default_dist = df_events.DefaultDistType.unique() 
-        return unique_default_dist        
-    
+        df_events = self.__get_event_data(dist_class, project_prefix)
+        unique_default_dist = df_events.DefaultDistType.unique()
+        return unique_default_dist
+
+
     def __build_project_events_query_string(self, project_DBPath, default_disturbances):
         qryStr = """
             select 
-                tDT.DefaultDistTypeID as DefaultDistType, 
+                tDT.DefaultDistTypeID as DefaultDistType,
                 tDT.DistTypeName,
-                (tDE.TimeStepStart + 1989) as HarvestYear,                  
-                tDT.DistTypeID,  
+                (tDE.TimeStepStart + 1989) as HarvestYear,
+                tDT.DistTypeID,
                 IIF(tDE.DistFormat = 1, 'DistArea', IIF(tDE.DistFormat = 2, 'MerchCarbonToDisturb', '')) as TargetType,
                 sum(IIF(tDE.DistFormat = 1, tDE.DistArea, tDE.MerchCarbonToDisturb)) AS ProjectTarget
-            from tblDisturbanceType as tDT inner join 
-                (tblDisturbanceEvents as tDE inner join 
+            from tblDisturbanceType as tDT inner join
+                (tblDisturbanceEvents as tDE inner join
                     (tblDisturbanceGroupScenario as tDGS inner join
                         (
-                        select distinct tRDSL.DisturbanceGroupScenarioID, tRDSL.RunDisturbanceScenarioID, tRDSL.DistTypeID, tRTD.RunLength 
-                        from tblRunDisturbanceScenarioLookup as tRDSL inner join 
+                        select distinct tRDSL.DisturbanceGroupScenarioID, tRDSL.RunDisturbanceScenarioID, tRDSL.DistTypeID, tRTD.RunLength
+                        from tblRunDisturbanceScenarioLookup as tRDSL inner join
                           (tblRunTable as tRT inner join tblRunTableDetails as tRTD on tRT.RunID = tRTD.RunID)
                         on tRT.RunDisturbanceScenarioID = tRDSL.RunDisturbanceScenarioID
                         in '{0}'
-                        where tRT.RunID = (select max(runid) from tblSimulation in '{0}')      
+                        where tRT.RunID = (select max(runid) from tblSimulation in '{0}')
                         ) as join1
                         on tDGS.DisturbanceGroupScenarioID = join1.DisturbanceGroupScenarioID
                     )
@@ -100,40 +104,40 @@ class DGChecker(object):
             )
             on tDT.DistTypeID = tDGS.DistTypeID
             in '{0}'
-            where tDT.DefaultDistTypeID in ({1}) and 
+            where tDT.DefaultDistTypeID in ({1}) and
                 tDE.TimeStepStart <= join1.RunLength  and
-                tDE.TimeStepStart + 1989 >= {2}  
-            group by                
-                tDT.DefaultDistTypeID, 
-                tDT.DistTypeID, 
-                tDT.DistTypeName, 
-                tDE.TimeStepStart, 
+                tDE.TimeStepStart + 1989 >= {2}
+            group by
+                tDT.DefaultDistTypeID,
+                tDT.DistTypeID,
+                tDT.DistTypeName,
+                tDE.TimeStepStart,
                 tDE.DistFormat
             order by
                 tDT.DefaultDistTypeID, tDE.TimeStepStart
-        """.format(project_DBPath, default_disturbances, self.event_start_year)        
-       
+        """.format(project_DBPath, default_disturbances, self.event_start_year)
+
         return qryStr
-    
+
+
     def check(self, project_prefix, project_path):
         project_dir = Path(project_path).parent
-        
-        #create qaqc log file  
+
+        #create qaqc log file
         with open(Path.joinpath(project_dir, "{0}_disturbance_generator_qaqc.csv".format(project_prefix)), 'a+') as output:
             for task in self.tasks:
-                dist_class = task["DisturbanceClass"]  
-                default_disturbance_types = self.__get_project_default_disturbance_ids(dist_class, project_prefix)  
+                dist_class = task["DisturbanceClass"]
+                default_disturbance_types = self.__get_project_default_disturbance_ids(dist_class, project_prefix)
                 str_default_distTypes =", " .join(str(id) for id in default_disturbance_types)
-                                
-                df_events = self.__get_event_data(dist_class, project_prefix)           
-                df_events = df_events.groupby(['ProjectName','DefaultDistType','TargetType', 'HarvestYear']).agg({"ProjectTarget": "sum"})    
-                
-                project_query = self.__build_project_events_query_string(project_path, str_default_distTypes)                
+
+                df_events = self.__get_event_data(dist_class, project_prefix)
+                df_events = df_events.groupby(['ProjectName','DefaultDistType','TargetType', 'HarvestYear']).agg({"ProjectTarget": "sum"})
+
+                project_query = self.__build_project_events_query_string(project_path, str_default_distTypes)
                 with AccessDB(project_path) as project_db:
-                    df_proj = pd.read_sql(project_query, project_db.connection)                      
-                    
+                    df_proj = pd.read_sql(project_query, project_db.connection)
+
                 df_check = pd.merge(df_events, df_proj, how='left', on=['DefaultDistType', 'HarvestYear'])
                 df_check['difference']= df_check['ProjectTarget_x'] - df_check['ProjectTarget_y']
-               
-                df_check.to_csv(output)   
-            
+
+                df_check.to_csv(output)
