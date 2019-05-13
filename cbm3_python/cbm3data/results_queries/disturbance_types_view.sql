0aedc5fe446b85fae516ed2858a683dce2c85b14
Scott Morken <smorken@nrcan.gc.ca>
2019-05-13 14:33:49 -0700

    default dist type name column in query

--- cbm3_python/cbm3data/results_queries/disturbance_types_view.sql
+++ cbm3_python/cbm3data/results_queries/disturbance_types_view.sql
@@ -2,7 +2,7 @@ SELECT
 tblDisturbanceType.DistTypeID,
 tblDisturbanceType.DefaultDistTypeID,
 tblDisturbanceType.DistTypeName,
-tblDisturbanceTypeDefault.DistTypeName
+tblDisturbanceTypeDefault.DistTypeName as DefaultDistTypeName
 FROM tblDisturbanceType 
 INNER JOIN tblDisturbanceTypeDefault ON 
 tblDisturbanceType.DefaultDistTypeID = tblDisturbanceTypeDefault.DistTypeID;
