24b23cdf41ae87bd3ed434b654f620685f8f077a
Scott Morken <scottm361@gmail.com>
2020-07-08 20:38:52 -0700

    disturbance types rollup view

--- cbm3_python/cbm3data/results_queries/disturbance_types_rollup_view.sql
+++ cbm3_python/cbm3data/results_queries/disturbance_types_rollup_view.sql
@@ -1,8 +1,4 @@
-SELECT
-tblDisturbanceType.DistTypeID,
-tblDisturbanceType.DefaultDistTypeID,
-tblDisturbanceType.DistTypeName,
-tblDisturbanceTypeDefault.DistTypeName as DefaultDistTypeName
-FROM tblDisturbanceType 
-INNER JOIN tblDisturbanceTypeDefault ON 
-tblDisturbanceType.DefaultDistTypeID = tblDisturbanceTypeDefault.DistTypeID;
+SELECT 
+tblDisturbanceTypeDefault.DistTypeID,
+tblDisturbanceTypeDefault.DistTypeName
+FROM tblDisturbanceTypeDefault;
