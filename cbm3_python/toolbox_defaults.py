1bf236b7bb13d7701b2641dfde0d05ae82ee99be
Scott Morken <scottm361@gmail.com>
2020-07-31 13:02:04 -0700

    add default toolbox paths

---
+++ cbm3_python/toolbox_defaults.py
@@ -0,0 +1,10 @@
+import os
+
+INSTALL_PATH = os.path.join(
+    "c:\\", "Program Files (x86)", "Operational-Scale CBM-CFS3")
+
+ARCHIVE_INDEX_PATH = os.path.join(
+    INSTALL_PATH, "Admin", "DBs", "ArchiveIndex_Beta_Install.mdb")
+
+CBM_EXECUTABLE_DIR = os.path.join(
+    INSTALL_PATH, "Admin", "Executables")
