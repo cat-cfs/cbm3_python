268e3d4c34bb1434c8aa481d08783d522858bc35
Scott Morken <scottm361@gmail.com>
2021-04-13 17:15:17 -0700

    add filenames and column names for all output useful output files

--- cbm3_python/cbm3data/cbm3_output_files.py
+++ cbm3_python/cbm3data/cbm3_output_files.py
@@ -90,32 +90,89 @@ def load_svl_files(dir, chunksize=None):
 
 
 def load_nir_output(dir, chunksize=None):
-    pass # NIROutput.txt
+    filename = "NIROutput.txt"
+    column_names = [
+        "TimeStep", "Year", "SPUID", "DistTypeID", "LandClass_From",
+        "LandClass_To", "DisturbedArea", "SWMerchC", "SWFoliageC", "SWOtherC",
+        "SWCoarseRootC", "SWFineRootC", "HWMerchC", "HWFoliageC", "HWOtherC",
+        "HWCoarseRootC", "HWFineRootC", "VeryFastCAG", "VeryFastCBG",
+        "FastCAG", "FastCBG", "MediumC", "SlowCAG", "SlowCBG", "SWSSnagC",
+        "HWSSnagC", "SWBSnagC", "HWBSnagC", "BlackC", "PeatC"]
+    pass
 
 
 def load_nodist(dir, chunksize=None):
+    filename = "nodist.fil"
+    column_names = [
+        "RunID", "TimeStep", "DistTypeID", "DistGroup", "UndisturbedArea"
+    ]
     pass
 
 
 def load_distseries(dir, chunksize=None):
+    filename = "distseries.csv"
+    # column headers are present in this csv file
+    columns_names = [
+        "timestep", "previous_kf5", "current_kf5", "area_disturbed"]
     pass
 
 
 def load_accdiagnostics(dir, chunksize=None):
-    pass
+    filename = "accdiagnostics.txt"
+    column_names = [
+        "id", "rule_type", "target", "target_value", "timestep", "action",
+        "distTypeID", "area", "age"
+    ]
 
 
 def load_predistage(dir, chunksize=None):
+    filename = "predistage.csv"
+    # column headers are present in this csv file
+    column_names = [
+        "spuid", "dist_type", "timestep",
+        "c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9",
+        "", "k0", "k1", "k2", "k3", "k4", "k5",
+        "pre_dist_age", "area_disturbed"]
     pass
 
 
 def load_seed(dir, chunksize=None):
+    filename = "seed.txt"
+    column_names = ["MonteCarloAssumptionID", "RunID", "RandomSeed"]
     pass
 
 
 def load_spatial_flux(dir, chunksize=None):
-    pass
+    filename = "spatialpool.out"
+    column_names = ["RunID", "SVOID", "Age", "TimeStep", "SPUID"] + \
+        get_classifier_column_names() + \
+        ["UNFCCC_ForestType", "KP33_34", "UNFCCC_Year", "KF33_Year",
+         "KFProjectType", "KFProjectID", "SWMerchC", "SWFoliageC", "SWOtherC",
+         "SWSubmerchC", "SWCoarseRootC", "SWFineRootC", "HWMerchC",
+         "HWFoliageC", "HWOtherC", "HWSubmerchC", "HWCoarseRootC",
+         "HWFineRootC", "VeryFastCAG", "VeryFastCBG", "FastCAG", "FastCBG",
+         "MediumC", "SlowCAG", "SlowCBG", "SWSSnagC", "SWBSnagC", "HWSSnagC",
+         "HWBSnagC", "BlackC", "PeatC"]
 
 
 def load_spatial_pools(dir, chunksize=None):
+    filename = "SpatialFluxInd.out"
+    column_names = ["RunID", "SVOID", "TimeStep", "SPUID", "DistTypeID"] + \
+        get_classifier_column_names() + \
+        ["UNFCCC_ForestType", "KP33_34", "UNFCCC_Year", "KF33_Year",
+         "KFProjectType", "KFProjectID", "CO2Production", "CH4Production",
+         "COProduction", "BioCO2Emission", "BioCH4Emission", "BioCOEmission",
+         "DOMCO2Emission", "DOMCH4Emission", "DOMCOEmission", "SoftProduction",
+         "HardProduction", "DOMProduction", "DeltaBiomass_AG",
+         "DeltaBiomass_BG", "DeltaDOM", "BiomassToSoil",
+         "LitterFlux_MERCHANTABLE", "LitterFlux_FOLIAGE", "LitterFlux_OTHER",
+         "LitterFlux_SUBMERCHANTABLE", "LitterFlux_COARSEROOT",
+         "LitterFlux_FINEROOT", "SoilToAir_VERYFASTAG", "SoilToAir_VERYFASTBG",
+         "SoilToAir_FASTAG", "SoilToAir_FASTBG", "SoilToAir_MEDIUM",
+         "SoilToAir_SLOWAG", "SoilToAir_SLOWBG", "SoilToAir_SSTEMSNAG",
+         "SoilToAir_SBRANCHSNAG", "SoilToAir_HSTEMSNAG",
+         "SoilToAir_HBRANCHSNAG", "SoilToAir_BLACKCARBON", "SoilToAir_PEAT",
+         "BioToAir_MERCHANTABLE", "BioToAir_FOLIAGE", "BioToAir_OTHER",
+         "BioToAir_SUBMERCHANTABLE", "BioToAir_COARSEROOT",
+         "BioToAir_FINEROOT"]
     pass
