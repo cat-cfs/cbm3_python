d998be1c46410c3b0c91ec62cf8d3cb0dd3c5744
Scott Morken <scottm361@gmail.com>
2021-04-14 14:49:28 -0700

    finish part of refactor

--- cbm3_python/cbm3data/cbm3_output_files_loader.py
+++ cbm3_python/cbm3data/cbm3_output_files_loader.py
@@ -1,95 +1,60 @@
 import os
+import copy
 import pandas as pd
-
+import functools
 from cbm3_python.cbm3data import cbm3_output_files
 from cbm3_python.cbm3data import cbm3_output_descriptions
 from cbm3_python.cbm3data import cbm3_output_classifiers
 from cbm3_python.cbm3data.cbm3_output_descriptions import ResultsDescriber
 
 
+LOAD_FUNCTIONS = {
+    "tblAgeIndicators": {
+        "load_function": cbm3_output_files.load_age_indicators,
+        "process_function": lambda loaded_csets, index_offset: _compose(
+            _get_replace_with_classifier_set_id_func(loaded_csets),
+            _get_drop_column_func("RunID"),
+            _get_column_rename_func(
+                _load_local_column_map("age_indicators_column_mapping.csv")),
+            _get_add_id_column_func("AgeIndID", index_offset))},
+    "tblDistIndicators": {
+        "load_function": cbm3_output_files.load_dist_indicators,
+        "process_function": lambda loaded_csets, index_offset: _compose(
+            _get_replace_with_classifier_set_id_func(loaded_csets),
+            _get_drop_column_func("RunID"),
+            _get_column_rename_func(
+                _load_local_column_map("dist_indicators_column_mapping.csv")),
+            _get_add_id_column_func("DistIndID", index_offset))},
+    "tblPoolIndicators": {
+        "load_function": cbm3_output_files.load_pool_indicators,
+        "process_function": lambda loaded_csets, index_offset: _compose(
+            _get_replace_with_classifier_set_id_func(loaded_csets),
+            _get_drop_column_func("RunID"),
+            _get_column_rename_func(
+                _load_local_column_map("pool_indicators_column_mapping.csv")),
+            _get_add_id_column_func("PoolIndID", index_offset))},
+    "tblFluxIndicators": {
+        "load_function": cbm3_output_files.load_flux_indicators,
+        "process_function": lambda loaded_csets, index_offset: _compose(
+            _get_replace_with_classifier_set_id_func(loaded_csets),
+            _get_drop_column_func("RunID"),
+            _get_column_rename_func(
+                _load_local_column_map("flux_indicators_column_mapping.csv")),
+            _get_add_id_column_func("FluxIndicatorID", index_offset),
+            _get_gross_growth_column_funcs())}
+    }
+
+DESCRIBE_FUNCTIONS = {
+    "tblAgeIndicators": "",
+    "tblDistIndicators": "",
+    "tblPoolIndicators": "",
+    "tblFluxIndicators": "",
+}
+
+
 def _get_local_file(filename):
     return os.path.join(
         os.path.dirname(os.path.realpath(__file__)), filename)
-
-
-def _process_pool_indicator_table(loaded_csets, pool_indicators, index_offset):
-    pool_indicators_new = \
-        cbm3_output_classifiers.replace_with_classifier_set_id(
-            pool_indicators, loaded_csets)
-    mapping_data = pd.read_csv(
-        _get_local_file("pool_indicators_column_mapping.csv"))
-    column_map = {
-        row.CBM3_raw_column_name: row.RRDB_column_name
-        for _, row in mapping_data.dropna().iterrows()}
-    pool_indicators_new.rename(columns=column_map, inplace=True)
-    pool_indicators_new.drop(columns="RunID", inplace=True)
-    pool_indicators_new.insert(
-        loc=0, column="PoolIndID",
-        value=pool_indicators_new.index + index_offset + 1)
-    return pool_indicators_new
-
-
-def _process_flux_indicator_table(loaded_csets, flux_indicators, index_offset):
-    flux_indicators_new = \
-        cbm3_output_classifiers.replace_with_classifier_set_id(
-            flux_indicators, loaded_csets)
-    mapping_data = pd.read_csv(
-        _get_local_file("flux_indicators_column_mapping.csv"))
-    column_map = {
-        row.CBM3_raw_column_name: row.RRDB_column_name
-        for _, row in mapping_data.dropna().iterrows()}
-    flux_indicators_new.rename(columns=column_map, inplace=True)
-    flux_indicators_new.drop(columns="RunID", inplace=True)
-    flux_indicators_new.insert(
-        loc=0, column="FluxIndicatorID",
-        value=flux_indicators_new.index + index_offset + 1)
-    # gross growth AG and BG are composite flux indicators that are not
-    # included in RAW CBM3 output, but are present in tblFluxIndicators
-    flux_indicators_new["GrossGrowth_AG"] = flux_indicators_new[
-        ["DeltaBiomass_AG", "MerchLitterInput", "FolLitterInput",
-         "OthLitterInput", "SubMerchLitterInput"]].sum(axis=1)
-    flux_indicators_new.loc[
-        flux_indicators_new.DistTypeID != 0, "GrossGrowth_AG"] = 0.0
-    flux_indicators_new["GrossGrowth_BG"] = flux_indicators_new[
-        ["DeltaBiomass_BG", "CoarseLitterInput", "FineLitterInput"]
-    ].sum(axis=1)
-    flux_indicators_new.loc[
-        flux_indicators_new.DistTypeID != 0, "GrossGrowth_BG"] = 0.0
-    return flux_indicators_new
-
-
-def _process_age_indicator_table(loaded_csets, age_indicators, index_offset):
-    age_indicators_new = \
-        cbm3_output_classifiers.replace_with_classifier_set_id(
-            age_indicators, loaded_csets)
-    mapping_data = pd.read_csv(
-        _get_local_file("age_indicators_column_mapping.csv"))
-    column_map = {
-        row.CBM3_raw_column_name: row.RRDB_column_name
-        for _, row in mapping_data.dropna().iterrows()}
-    age_indicators_new.rename(columns=column_map, inplace=True)
-    age_indicators_new.drop(columns="RunID", inplace=True)
-    age_indicators_new.insert(
-        loc=0, column="AgeIndID",
-        value=age_indicators_new.index + index_offset + 1)
-    return age_indicators_new
-
-
-def _process_dist_indicator_table(loaded_csets, dist_indicators, index_offset):
-    dist_indicators_new = \
-        cbm3_output_classifiers.replace_with_classifier_set_id(
-            dist_indicators, loaded_csets)
-    mapping_data = pd.read_csv(
-        _get_local_file("dist_indicators_column_mapping.csv"))
-    column_map = {
-        row.CBM3_raw_column_name: row.RRDB_column_name
-        for _, row in mapping_data.dropna().iterrows()}
-    dist_indicators_new.rename(columns=column_map, inplace=True)
-    dist_indicators_new.drop(columns="RunID", inplace=True)
-    dist_indicators_new.insert(
-        loc=0, column="DistIndID",
-        value=dist_indicators_new.index + index_offset + 1)
-    return dist_indicators_new
 
 
 def _load_local_column_map(filename):
@@ -129,7 +94,34 @@ def _get_add_id_column_func(id_column_name, index_offset):
     return func
 
 
-def _get_load_functions():
+def _get_gross_growth_column_funcs():
+    def func(df):
+        # gross growth AG and BG are composite flux indicators that are not
+        # included in RAW CBM3 output, but are present in tblFluxIndicators
+        df["GrossGrowth_AG"] = df[
+            ["DeltaBiomass_AG", "MerchLitterInput", "FolLitterInput",
+            "OthLitterInput", "SubMerchLitterInput"]].sum(axis=1)
+        df.loc[df.DistTypeID != 0, "GrossGrowth_AG"] = 0.0
+        df["GrossGrowth_BG"] = df[
+            ["DeltaBiomass_BG", "CoarseLitterInput", "FineLitterInput"]
+        ].sum(axis=1)
+        df.loc[df.DistTypeID != 0, "GrossGrowth_BG"] = 0.0
+        return df
+    return func
+
+
+def _compose(*fs):
+    """Adapted from:
+    https://stackoverflow.com/questions/16739290/composing-functions-in-python
+    """
+    def compose2(f, g):
+        return lambda *a, **kw: f(g(*a, **kw))
+
+    return functools.reduce(compose2, fs)
+
+
+def _get_load_functions(table_name, descriptive=False):
+    return LOAD_FUNCTIONS[table_name]
 
 
 def load_output_relational_tables(cbm_run_results_dir, project_db_path,
@@ -152,37 +144,23 @@ def load_output_relational_tables(cbm_run_results_dir, project_db_path,
     for k, v in project_data.__dict__.items():
         out_func(k, v)
 
-    results_list = [
-        {"table_name": "tblAgeIndicators",
-         "load_function": cbm3_output_files.load_age_indicators,
-         "process_functions": [
-             _get_replace_with_classifier_set_id_func(loaded_csets),
-             _get_drop_column_func("RunID"),
-             _get_column_rename_func(
-                 _load_local_column_map("dist_indicators_column_mapping.csv")),
-             _get_add_id_column_func("AgeIndID", )
-         ]},
-        {"table_name": "tblDistIndicators",
-         "load_function": cbm3_output_files.load_dist_indicators,
-         "process_functions": _process_dist_indicator_table},
-        {"table_name": "tblPoolIndicators",
-         "load_function": cbm3_output_files.load_pool_indicators,
-         "process_functions": _process_pool_indicator_table},
-        {"table_name": "tblFluxIndicators",
-         "load_function": cbm3_output_files.load_flux_indicators,
-         "process_functions": _process_flux_indicator_table}]
-
-    for result_item in results_list:
+    results_table_list = [
+        "tblAgeIndicators",
+        "tblDistIndicators",
+        "tblPoolIndicators",
+        "tblFluxIndicators"
+        ]
+    for table_name in results_table_list:
+        load_functions = _get_load_functions(table_name)
         result_chunk_iterable = cbm3_output_files.make_iterable(
-            result_item["load_function"], cbm_run_results_dir, chunksize)
+            load_functions["load_function"], cbm_run_results_dir, chunksize)
         index_offset = 0
         for chunk in result_chunk_iterable:
-            processed_chunk = result_item["process_function"](
-                loaded_csets, chunk, index_offset)
+            process_function = load_functions["process_function"](
+                loaded_csets, index_offset)
+            processed_chunk = process_function(chunk)
             index_offset = index_offset + len(processed_chunk.index)
-            out_func(
-                result_item["table_name"],
-                processed_chunk)
+            out_func(table_name, processed_chunk)
 
 
 def load_output_descriptive_tables(cbm_run_results_dir, project_db_path,
