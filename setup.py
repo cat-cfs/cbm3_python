5e9dd8d3cbf371194f0a0896ba889aed844d350b
Scott Morken <scottm361@gmail.com>
2021-04-23 15:32:21 -0700

    new ver 0.8.6

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ console_scripts = [
 
 setup(
     name="cbm3_python",
-    version="0.8.5",
+    version="0.8.6",
     description="Scripts to automate tasks with CBM-CFS3",
     keywords=["cbm-cfs3"],
     long_description=long_description,
