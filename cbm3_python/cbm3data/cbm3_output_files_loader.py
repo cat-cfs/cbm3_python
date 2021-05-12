26aa83bff80790a93c55035ae0b1194a5b0dbb54
Scott Morken <scottm361@gmail.com>
2021-05-12 13:37:19 -0700

    formatting

--- cbm3_python/cbm3data/cbm3_output_files_loader.py
+++ cbm3_python/cbm3data/cbm3_output_files_loader.py
@@ -309,7 +309,8 @@ def _get_cbm_input_dir(cbm_output_dir):
 
 def load_output_relational_tables(cbm_output_dir, project_db_path,
                                   aidb_path, out_func, chunksize=None,
-                                  include_spatial=False, include_diagnostics=False):
+                                  include_spatial=False,
+                                  include_diagnostics=False):
 
     project_data = cbm3_output_descriptions.load_project_level_data(
         project_db_path)
