3c7da3674542a2fa558f88dbb5c9ed65167312d0
Scott Morken <scottm361@gmail.com>
2021-05-19 14:46:14 -0700

    docs

--- cbm3_python/cbm3data/cbm3_output_classifiers.py
+++ cbm3_python/cbm3data/cbm3_output_classifiers.py
@@ -5,6 +5,32 @@ from warnings import warn
 
 def create_loaded_classifiers(tblClassifiers, tblClassifierSetValues,
                               cbm_results_dir, chunksize=None):
+    """Assembles a dataframe of classifier value ids using both the CBM
+    input and CBM output data.  This is required since CBM output can contain
+    classifier sets that do not exist in the input.
+
+    Args:
+        tblClassifiers (pandas.DataFrame): tblClassifers as queried from a
+            project database.
+        tblClassifierSetValues (pandas.DataFrame): tblClassifierSetValues as
+            queried from a project database.
+        cbm_results_dir (str): directory containing CBM text results
+            (the CBMRun/output dir)
+        chunksize (int, optional): If specified sets a maximum number of rows
+            to hold in memory at a given time while loading output. If
+            unspecified there is no limit. Defaults to None.
+
+    Returns:
+        pandas.DataFrame: A table of classifier value ids for each classifier
+            set id/classifier ID defined both in the project data and the
+            output text data in pivoted form. The columns are:
+
+            * ClassifierSetID
+            * ClassifierID1
+            * ClassifierID2
+            * ...
+            * ClassifierIDN
+    """
     if (tblClassifierSetValues.ClassifierValueID < 0).any():
         warn("tblClassifierSetValues.ClassifierValueID: < 0 values detected")
         tblClassifierSetValues.loc[
@@ -53,6 +79,21 @@ def create_loaded_classifiers(tblClassifiers, tblClassifierSetValues,
 
 
 def replace_with_classifier_set_id(raw_table, cset_pivot):
+    """Removes the columns named c1, c2, ... c10 from the specified table.
+    Adds a UserDefdClassSetID column with the classifier set id for each row
+    in the raw data.
+
+    The provided raw_table is modified in-place and not copied.
+
+    Args:
+        raw_table (pandas.DataFrame): A raw parsed CBM output table that
+            contains the c1, ..., c10 classifier value columns.
+        cset_pivot (pandas.DataFrame): A table of classifier value ids by
+            classifier set id (rows) and classifier id (columns).
+
+    Returns:
+        pandas.DataFrame: the processed raw table
+    """
     classifier_set_insertion_index = raw_table.columns.get_loc("c1")
     raw_cset_columns = [f"c{x+1}" for x in range(0, len(cset_pivot.columns)-1)]
     cset_pivot_out = cset_pivot.drop_duplicates(
@@ -72,6 +113,28 @@ def replace_with_classifier_set_id(raw_table, cset_pivot):
 
 
 def melt_loaded_csets(csets):
+    """Melts classifier sets in pivot form to long form, with columns:
+
+        * ClassifierSetID
+        * ClassifierID
+        * ClassifierValueID
+
+    Any null values in in the pivoted data will be replaced with 1 which
+    is the wildcard classifier value id by convention.
+
+    Args:
+        csets (pandas.DataFrame): The pivoted classifier set table. Each value
+        in the table is a classifier value id.  The columns are as follows:
+
+            * ClassifierSetID
+            * ClassifierID1
+            * ClassifierID2
+            * ...
+            * ClassifierIDN
+
+    Returns:
+        pandas.DataFrame: The melted classifier sets.
+    """
 
     csets_melt = csets.copy()
     csets_melt.columns = \
