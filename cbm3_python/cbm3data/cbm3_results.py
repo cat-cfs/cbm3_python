20afee5b89fad08ef5ee86140bc2ac63abd41047
Scott Morken <scottm361@gmail.com>
2021-05-20 19:11:14 -0700

    start of refactor to support other database types

--- cbm3_python/cbm3data/cbm3_results.py
+++ cbm3_python/cbm3data/cbm3_results.py
@@ -1,39 +1,47 @@
 import pandas as pd
-from cbm3_python.cbm3data import accessdb
+
 from cbm3_python.cbm3data import results_queries
 
 
-def get_classifier_values(results_path):
+def _load_df(sql, results_db):
+    from cbm3_python.cbm3data import accessdb
+    if isinstance(results_db, str):
+        return accessdb.as_data_frame(sql, results_db)
+    else:
+        return pd.read_sql(sql, results_db)
+
+
+def get_classifier_values(results_db):
     '''
     loads the classifier values in the specified results database into an
     indexed collection to serve for labels, grouping and filtering CBM results
     tables
     '''
     sql = results_queries.get_classifiers_view()
-    df = accessdb.as_data_frame(sql, results_path)
+    df = _load_df(sql, results_db)
     return df.pivot(
         index="UserDefdClassSetID", columns="ClassDesc",
         values="UserDefdSubClassName")
 
 
-def load_pool_indicators(results_db_path,
+def load_pool_indicators(results_db,
                          spatial_unit_grouping=False,
                          classifier_set_grouping=False,
                          land_class_grouping=False,
                          rollup_format=False):
     sql = results_queries.get_pool_indicators_view_sql(
         spatial_unit_grouping, classifier_set_grouping, land_class_grouping)
-    df = accessdb.as_data_frame(sql, results_db_path)
+    df = _load_df(sql, results_db)
     if classifier_set_grouping:
-        df = join_classifiers(df, get_classifier_values(results_db_path))
+        df = _join_classifiers(df, get_classifier_values(results_db))
     if spatial_unit_grouping:
-        df = join_spatial_units(df, accessdb.as_data_frame(
+        df = _join_spatial_units(df, _load_df(
             results_queries.get_spatial_units_view(rollup_format),
-            results_db_path))
+            results_db))
     return df
 
 
-def load_stock_changes(results_db_path,
+def load_stock_changes(results_db,
                        disturbance_type_grouping=False,
                        spatial_unit_grouping=False,
                        classifier_set_grouping=False,
@@ -42,21 +50,21 @@ def load_stock_changes(results_db_path,
     sql = results_queries.get_stock_changes_view(
         disturbance_type_grouping, spatial_unit_grouping,
         classifier_set_grouping, land_class_grouping)
-    df = accessdb.as_data_frame(sql, results_db_path)
+    df = _load_df(sql, results_db)
     if classifier_set_grouping:
-        df = join_classifiers(df, get_classifier_values(results_db_path))
+        df = _join_classifiers(df, get_classifier_values(results_db))
     if spatial_unit_grouping:
-        df = join_spatial_units(df, accessdb.as_data_frame(
+        df = _join_spatial_units(df, _load_df(
             results_queries.get_spatial_units_view(rollup_format),
-            results_db_path))
+            results_db))
     if disturbance_type_grouping:
-        df = join_disturbance_types(df, accessdb.as_data_frame(
+        df = _join_disturbance_types(df, _load_df(
             results_queries.get_disturbance_types_view(rollup_format),
-            results_db_path))
+            results_db))
     return df
 
 
-def load_flux_indicators(results_db_path,
+def load_flux_indicators(results_db,
                          disturbance_type_grouping=False,
                          spatial_unit_grouping=False,
                          classifier_set_grouping=False,
@@ -65,21 +73,21 @@ def load_flux_indicators(results_db_path,
     sql = results_queries.get_flux_indicators_view(
         disturbance_type_grouping, spatial_unit_grouping,
         classifier_set_grouping, land_class_grouping)
-    df = accessdb.as_data_frame(sql, results_db_path)
+    df = _load_df(sql, results_db)
     if classifier_set_grouping:
-        df = join_classifiers(df, get_classifier_values(results_db_path))
+        df = _join_classifiers(df, get_classifier_values(results_db))
     if spatial_unit_grouping:
-        df = join_spatial_units(df, accessdb.as_data_frame(
+        df = _join_spatial_units(df, _load_df(
             results_queries.get_spatial_units_view(rollup_format),
-            results_db_path))
+            results_db))
     if disturbance_type_grouping:
-        df = join_disturbance_types(df, accessdb.as_data_frame(
+        df = _join_disturbance_types(df, _load_df(
             results_queries.get_disturbance_types_view(rollup_format),
-            results_db_path))
+            results_db))
     return df
 
 
-def load_age_indicators(results_db_path,
+def load_age_indicators(results_db,
                         spatial_unit_grouping=False,
                         classifier_set_grouping=False,
                         land_class_grouping=False,
@@ -87,17 +95,17 @@ def load_age_indicators(results_db_path,
     sql = results_queries.get_age_indicators_view_sql(
         spatial_unit_grouping, classifier_set_grouping,
         land_class_grouping)
-    df = accessdb.as_data_frame(sql, results_db_path)
+    df = _load_df(sql, results_db)
     if classifier_set_grouping:
-        df = join_classifiers(df, get_classifier_values(results_db_path))
+        df = _join_classifiers(df, get_classifier_values(results_db))
     if spatial_unit_grouping:
-        df = join_spatial_units(df, accessdb.as_data_frame(
+        df = _join_spatial_units(df, _load_df(
             results_queries.get_spatial_units_view(rollup_format),
-            results_db_path))
+            results_db))
     return df
 
 
-def load_disturbance_indicators(results_db_path,
+def load_disturbance_indicators(results_db,
                                 disturbance_type_grouping=False,
                                 spatial_unit_grouping=False,
                                 classifier_set_grouping=False,
@@ -106,32 +114,32 @@ def load_disturbance_indicators(results_db_path,
     sql = results_queries.get_disturbance_indicators_view_sql(
         disturbance_type_grouping, spatial_unit_grouping,
         classifier_set_grouping, land_class_grouping)
-    df = accessdb.as_data_frame(sql, results_db_path)
+    df = _load_df(sql, results_db)
     if classifier_set_grouping:
-        df = join_classifiers(df, get_classifier_values(results_db_path))
+        df = _join_classifiers(df, get_classifier_values(results_db))
     if spatial_unit_grouping:
-        df = join_spatial_units(df, accessdb.as_data_frame(
+        df = _join_spatial_units(df, _load_df(
             results_queries.get_spatial_units_view(rollup_format),
-            results_db_path))
+            results_db))
     if disturbance_type_grouping:
-        df = join_disturbance_types(df, accessdb.as_data_frame(
+        df = _join_disturbance_types(df, _load_df(
             results_queries.get_disturbance_types_view(rollup_format),
-            results_db_path))
+            results_db))
     return df
 
 
-def join_classifiers(indicators, classifiers):
+def _join_classifiers(indicators, classifiers):
     return pd.merge(
         indicators, classifiers,
         left_on="UserDefdClassSetID",
         right_on="UserDefdClassSetID")
 
 
-def join_spatial_units(indicators, spatial_units):
+def _join_spatial_units(indicators, spatial_units):
     return pd.merge(indicators, spatial_units,
                     left_on="SPUID", right_on="SPUID")
 
 
-def join_disturbance_types(indicators, disturbance_types):
+def _join_disturbance_types(indicators, disturbance_types):
     return pd.merge(indicators, disturbance_types,
                     left_on="DistTypeID", right_on="DistTypeID")
