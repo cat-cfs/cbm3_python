fa59e820b92dd1bc675dd5b2f81de8803d83fc85
Scott Morken <scottm361@gmail.com>
2021-05-27 11:13:24 -0700

    remove requirement for "results_database_path" keyword arg (which was only being used to set the path for log file) and add support for explicit log_path

--- cbm3_python/simulation/concurrent_runner.py
+++ cbm3_python/simulation/concurrent_runner.py
@@ -19,24 +19,30 @@ class ConcurrentRunner:
         # the following args that are optional in the
         # non-concurrent run function are required here
         required_kwargs = [
-            "aidb_path", "cbm_exe_path", "results_database_path"]
+            "aidb_path", "cbm_exe_path"]
         for required_kwarg in required_kwargs:
             if required_kwarg not in run_args:
                 raise ValueError(f"{required_kwarg} is a required argument")
-        results_database_dir = os.path.dirname(
-            run_args["results_database_path"])
-        if not os.path.exists(results_database_dir):
-            os.makedirs(results_database_dir)
-        log_path = os.path.splitext(
-            run_args["results_database_path"])[0] + ".log"
-        loghelper.start_logging(log_path, 'w+', use_console=False)
+
+        log_path = None
+        if "log_path" in run_args and run_args["log_path"]:
+            log_path = run_args["log_path"]
+
+            if not os.path.exists(os.path.dirname(log_path)):
+                os.makedirs(os.path.dirname(log_path))
+                loghelper.start_logging(
+                    run_args["log_path"], 'w+', use_console=False)
+
         with tempfile.TemporaryDirectory() as temp_dir:
             toolbox_env_path = os.path.join(temp_dir, "toolbox")
             toolbox_env.create_toolbox_env(
                 self.toolbox_path, toolbox_env_path)
 
             kwargs = {
-                k: v for k, v in run_args.items() if k != "project_path"}
+                k: v for k, v in run_args.items()
+                # these items are not keyword args for
+                # projectsimulator.run
+                if k not in ["project_path", "log_path"]}
             kwargs["toolbox_installation_dir"] = toolbox_env_path
 
             # need to make a local copy of the archive index and project db,
@@ -54,7 +60,6 @@ class ConcurrentRunner:
             shutil.copy(run_args["project_path"], local_project_db)
             args = [local_project_db]
             projectsimulator.run(*args, **kwargs)
-            run_args["log_path"] = log_path
             return run_args
 
     def run_func(self, run_args):
