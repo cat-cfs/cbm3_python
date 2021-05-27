ed9b383f1a93259a3f8cfefefe0c9a742af1a134
Scott Morken <scottm361@gmail.com>
2021-05-27 11:21:09 -0700

    add cbm3_python logger, docs

--- cbm3_python/util/loghelper.py
+++ cbm3_python/util/loghelper.py
@@ -4,14 +4,21 @@
 import logging
 
 
-def start_logging(fn=".\\script.log", fmode='w', use_console=True):
-    #set up logging to print to console window and to log file
-    #
-    # From http://docs.python.org/2/howto/logging-cookbook.html#logging-cookbook
-    #
+def start_logging(fn, fmode='w', use_console=True):
+    """Start logging to a file and optionally to the console window
+
+    Args:
+        fn (str): Path to the log file.
+        fmode (str, optional): File mode string passed to logging.FileHandler.
+            Defaults to 'w'.
+        use_console (bool, optional): If set to true the logging output
+            is written to console. Defaults to True.
+    """
     rootLogger = logging.getLogger()
 
-    logFormatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M')
+    logFormatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M')
 
     fileHandler = logging.FileHandler(fn, fmode)
     fileHandler.setFormatter(logFormatter)
@@ -22,3 +29,12 @@ def start_logging(fn=".\\script.log", fmode='w', use_console=True):
         rootLogger.addHandler(consoleHandler)
 
     rootLogger.setLevel(logging.INFO)
+
+
+def get_logger():
+    """Gets the "cbm3_python" logger
+
+    Returns:
+        Logger: the "cbm3_python" logging.Logger
+    """
+    return logging.getLogger("cbm3_python")
